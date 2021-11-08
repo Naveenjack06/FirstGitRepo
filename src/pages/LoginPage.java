79708c55470854c6a9e232952a0a85a12309bcfd
Naveenjack06 <naveenjack.naveen@gmailcom>
2021-11-09 02:55:43 +0530

    First commit

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public final class LoginPage {
+
+	public LoginPage() {
+		System.out.println("Credntials");
+
+		System.out.println("Username" + "Admin");
+
+		System.out.println("Password" + "admin123");
+
+		System.out.println("Login sucess");
+
+	}
+}
---
+++ src/test/LoginTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import pages.LoginPage;
+
+public class LoginTest {
+	
+	public static void main(String[] args) {
+		
+		LoginPage login = new LoginPage();
+	}
+
+}
